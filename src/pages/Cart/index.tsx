1b71f22961e6f5ea09853ba0a46102b61792ca8c
Tierry Ray <tierry.ray@ifood.com.br>
2021-03-21 15:26:47 -0300

    feat(cart): add total logic

--- src/pages/Cart/index.tsx
+++ src/pages/Cart/index.tsx
@@ -26,12 +26,12 @@ const Cart = (): JSX.Element => {
     priceFormatted: formatPrice(product.price),
   }));
 
-  // const total =
-  //   formatPrice(
-  //     cart.reduce((sumTotal, product) => {
-  //       // TODO
-  //     }, 0)
-  //   )
+  const total =
+    formatPrice(
+      cart.reduce((sumTotal, product) => {
+        return sumTotal += product.price * product.amount
+      }, 0)
+    );
 
   function handleProductIncrement(product: Product) {
     // TODO
@@ -114,7 +114,7 @@ const Cart = (): JSX.Element => {
 
         <Total>
           <span>TOTAL</span>
-          <strong>R$ 359,80</strong>
+          <strong>{total}</strong>
         </Total>
       </footer>
     </Container>
